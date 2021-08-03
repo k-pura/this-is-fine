a041976d5d54b43bc56540a8eb46410f0c597250
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-08-03 12:59:01 -0400

    add index function

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -6,6 +6,11 @@ const thoughtsCtrl = require('../../controllers/api/thoughts');
 //POST a new thought record 
 router.post('/submitThoughtRecord', thoughtsCtrl.create)
 
+//SHOW one thought record
+router.get('/thoughts/:id/', thoughtsCtrl.show)
+
+//SHOW ALL THOUGHTS
+router.get('/thoughts', thoughtsCtrl.index)
 
 
 module.exports = router;
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
 import React from 'react';
 import NewThoughtRecordForm from '../../components/NewThoughtRecordForm/NewThoughtRecordForm';
+import MyThoughts from '../MyThoughts/MyThoughts';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,9 @@ export default class App extends Component {
        App
 
        <NewThoughtRecordForm/>
+
+
+       <MyThoughts /> 
       </main>
     )
   }

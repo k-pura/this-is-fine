924adad346c2c58fcbf88c5db68cf55544e70d29
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-08-03 21:08:03 -0400

    thought detail working

--- src/pages/ThoughtDetail/ThoughtDetail.jsx
+++ src/pages/ThoughtDetail/ThoughtDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default class ThoughtDetail extends Component {
   state = {
-    oneThought: [],
+    oneThought: {},
   } 
 
 //   getOne = async (incoming_thought_id) => {
@@ -16,8 +16,10 @@ export default class ThoughtDetail extends Component {
 
   async componentDidMount() {
     console.log("LOOK HERE", this.props.match.params.id)
+    let id = this.props.match.params.id
+    console.log()
     try {
-        let fetchThoughtDataResponse = await fetch('/api/thoughts/:id')
+        let fetchThoughtDataResponse = await fetch(`/api/thoughts/${id}`)
         if (!fetchThoughtDataResponse.ok) throw new Error("Couldn't fetch your thought!")
         let oneThoughtData = await fetchThoughtDataResponse.json() // <------- convert fetch response into a js object
         console.log("get thought", oneThoughtData)
@@ -30,7 +32,38 @@ export default class ThoughtDetail extends Component {
     return (
       <div className="thoughtDetail">
         <h1>Thought Detail</h1>
-    {this.state.oneThought.map(t => <h1>{t.entryName}</h1>)}
+            <p>Entry Name:</p>       
+            {this.state.oneThought.entryName}
+
+            <p>What emoji resonantes with you right now?</p>
+            {this.state.oneThought.emoji}
+
+            <p>What was the situation?</p>
+            {this.state.oneThought.situation}
+
+            <p>Describe your emotions or feelings:</p>
+            {this.state.oneThought.emotion}
+
+            <p>Automatic thoughts:</p>
+            {this.state.oneThought.automaticThoughts}
+
+            <p>Evidence supporting those thoughts:</p>
+            {this.state.oneThought.evidenceSupport}
+
+            <p>Evidence against those thoughts:</p>
+            {this.state.oneThought.evidenceAgainst}
+
+            <p>New balanced thought:</p>
+            {this.state.oneThought.balanceThought}
+
+            <p>How do you feel now?</p>
+            {this.state.oneThought.feelNow}
+            <br/>
+            <br/>
+            <button>Edit this thought record</button>
+            <br/>    
+            <br/>  
+            <button>Delete this thought record</button>
       </div>
     )
   }

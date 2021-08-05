ab8ba4bc71345266939cb3f0d7e5744a2078a1bf
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-08-04 23:56:24 -0400

    dad joke API

--- src/pages/Mindlessness/Mindlessness.jsx
+++ src/pages/Mindlessness/Mindlessness.jsx
@@ -2,10 +2,56 @@ import { Component } from 'react';
 import React from 'react';
 
 export default class Resources extends React.Component {
-    render(){
-        return(
-        <div>
-            <h1>FUN ZONE!</h1>
-        </div>
-    )}
+
+    state = {
+        joke: "",
+    }
+
+    async fetchJoke() {
+        try {
+            console.log("i am inside the fetch")
+            const response = await fetch("http://icanhazdadjoke.com", {
+                headers: {
+                  Accept: "application/json",
+                },
+              });
+              console.log("i have done headers")
+              const data = await response.json();
+              console.log("here the joke", data)
+
+        } catch (err) {
+          console.error('ERROR:', err) // <-- log if error
+        }
+      };
+
+      async componentDidMount() {
+        try {
+         await this.fetchJoke();
+        } catch (err) {
+          console.error('ERROR:', err) // <-- log if error
+        }
+      };
+
+      updateJoke (incoming_joke) {
+
+          this.setState ({
+              joke: incoming_joke
+          });
+      }
+
+
+        render(){
+            return(
+            <div className="mindless">
+                
+
+                <button onClick={()=>this.updateJoke()}>Click here for a new dad joke!</button>
+
+                <p>joke: {this.state.joke}</p>
+
+            </div>
+            )
+        }
+        
+
 }
